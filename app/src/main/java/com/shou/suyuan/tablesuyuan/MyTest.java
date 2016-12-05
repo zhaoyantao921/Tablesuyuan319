3075b56049fc6bd0b248d18d26146956f4570bf9
zhaoyantao <seeshou@163.com>
2016-12-05 11:27:30 +0800

    zhoayantao

---
+++ app/src/main/java/com/shou/suyuan/tablesuyuan/MyTest.java
@@ -0,0 +1,21 @@
+package com.shou.suyuan.tablesuyuan;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by zhao on 2016/12/5.
+ */
+
+public class MyTest extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
